3509b0e0d5374bb11bb76a6f0115b510600f75ae
Rajsingh6389 <rm2739159@gmail.com>
2025-11-24 19:13:00 +0530

    render

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "https://metalmarketserver-production.up.railway.app/api",
+  baseURL: "https://metalmarket-server-2.onrender.com/api",
+  // baseURL: "https://metalmarketserver-production.up.railway.app/api",
+
 });
 
 API.interceptors.request.use((config) => {
